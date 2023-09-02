1f82864949cbc17f51c0a609e63fdb71af2a2db0
tome <tamoszdev@gmail.com>
2023-09-03 10:40:24 +1200

    more boilerplate

--- commands.js
+++ commands.js
@@ -4,40 +4,40 @@ import { capitalize, InstallGlobalCommands } from './utils.js';
 
 // Get the game choices from game.js
 function createCommandChoices() {
-    const choices = getRPSChoices();
-    const commandChoices = [];
+  const choices = getRPSChoices();
+  const commandChoices = [];
 
-    for (let choice of choices) {
-        commandChoices.push({
-            name: capitalize(choice),
-            value: choice.toLowerCase(),
-        });
-    }
+  for (let choice of choices) {
+    commandChoices.push({
+      name: capitalize(choice),
+      value: choice.toLowerCase(),
+    });
+  }
 
-    return commandChoices;
+  return commandChoices;
 }
 
 // Simple test command
 const TEST_COMMAND = {
-    name: 'test',
-    description: 'Basic command',
-    type: 1,
+  name: 'test',
+  description: 'Basic command',
+  type: 1,
 };
 
 // Command containing options
 const CHALLENGE_COMMAND = {
-    name: 'challenge',
-    description: 'Challenge to a match of rock paper scissors',
-    options: [
-        {
-            type: 3,
-            name: 'object',
-            description: 'Pick your object',
-            required: true,
-            choices: createCommandChoices(),
-        },
-    ],
-    type: 1,
+  name: 'challenge',
+  description: 'Challenge to a match of rock paper scissors',
+  options: [
+    {
+      type: 3,
+      name: 'object',
+      description: 'Pick your object',
+      required: true,
+      choices: createCommandChoices(),
+    },
+  ],
+  type: 1,
 };
 
 const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND];
