e7740f823858c0725b033c8d0b4e24e8e66c4d63
tome <tamoszdev@gmail.com>
2023-10-08 11:26:41 +1300

    Restructuring for serverless AWS architecture

---
+++ src/app/deployed/lambda.py
@@ -0,0 +1,58 @@
+import json
+import dotenv
+
+from nacl.signing import VerifyKey
+from nacl.exceptions import BadSignatureError
+
+PUBLIC_KEY = "58ac805cd0fcebe4308ed2825c9dc3d9e801a3afdeed2ebe91d5c4a327539fc0"
+
+
+def lambda_handler(event, context):
+    try:
+        body = json.loads(event["body"])
+
+        signature = event["headers"]["x-signature-ed25519"]
+        timestamp = event["headers"]["x-signature-timestamp"]
+
+        # validate the interaction
+
+        verify_key = VerifyKey(bytes.fromhex(PUBLIC_KEY))
+
+        message = timestamp + json.dumps(body, separators=(",", ":"))
+
+        try:
+            verify_key.verify(message.encode(), signature=bytes.fromhex(signature))
+        except BadSignatureError:
+            return {"statusCode": 401, "body": json.dumps("invalid request signature")}
+
+        # handle the interaction
+
+        t = body["type"]
+
+        if t == 1:
+            return {"statusCode": 200, "body": json.dumps({"type": 1})}
+        elif t == 2:
+            return command_handler(body)
+        else:
+            return {"statusCode": 400, "body": json.dumps("unhandled request type")}
+    except:
+        raise
+
+
+def command_handler(body):
+    command = body["data"]["name"]
+
+    if command == "test":
+        return {
+            "statusCode": 200,
+            "body": json.dumps(
+                {
+                    "type": 4,
+                    "data": {
+                        "content": "Hello, World.",
+                    },
+                }
+            ),
+        }
+    else:
+        return {"statusCode": 400, "body": json.dumps("unhandled command")}
---
+++ src/app/local/commands.py
@@ -0,0 +1,17 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+APP_ID = os.getenv('APP_ID')
+SERVER_ID = os.getenv('SERVER_ID')
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+url = f"https://discord.com/api/v10/applications/{APP_ID}/guilds/{SERVER_ID}/commands"
+
+json = [{"name": "test", "description": "Test command.", "options": []}]
+
+response = requests.put(url, headers={"Authorization": f"Bot {BOT_TOKEN}"}, json=json)
+
+print(response.json())
